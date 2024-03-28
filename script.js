5d9575ef7572abe00aedcde0881302e2290c22bd
AmanKaushik975 <91477500+AmanKaushik975@users.noreply.github.com>
2024-03-29 00:14:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
-    //Write your code here
+    // Get all the <strong> elements
+    let boldItems = document.getElementsByTagName('strong');
 
-
+    // Loop through all the <strong> elements
+    for(let i = 0; i < boldItems.length; i++) {
+        // Change the color of the text to green
+        boldItems[i].style.color = 'green';
+    }
 }
 
-
 function return_normal() {
-    //Write your code here
+    // Get all the <strong> elements
+    let boldItems = document.getElementsByTagName('strong');
 
-    
+    // Loop through all the <strong> elements
+    for(let i = 0; i < boldItems.length; i++) {
+        // Change the color of the text back to black
+        boldItems[i].style.color = 'black';
+    }
 }
